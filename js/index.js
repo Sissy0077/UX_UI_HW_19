844f8c2b987dc6c45cf6bdc0f30476d36b83cb93
Antsis0077 <antsis0077@users.noreply.github.com>
2021-02-09 18:34:34 +1030

    UXUI_WK19_9.2.21_1834

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $("#hide").click(function(){
+    $("p1").hide();
+  });
+  $("#show").click(function(){
+    $("p1").show();
+  });
+});
+
+$(document).ready(function(){
+  $("#hide1").click(function(){
+    $("p2").hide();
+  });
+  $("#show1").click(function(){
+    $("P2").show();
+  });
+});
+
+$(document).ready(function(){
+  $("#hide2").click(function(){
+    $("p3").hide();
+  });
+  $("#show2").click(function(){
+    $("p3").show();
+  });
+});
+console.log("Your index.js file is loaded correctly!");
